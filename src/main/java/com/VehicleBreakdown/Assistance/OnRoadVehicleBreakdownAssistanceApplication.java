df7601c235bbc94e5998ec251eb1d276138f2162
Ayush Kesarwani <kaayush112@gmail.com>
2022-04-13 15:27:53 +0530

    Initial Commit

---
+++ src/main/java/com/VehicleBreakdown/Assistance/OnRoadVehicleBreakdownAssistanceApplication.java
@@ -0,0 +1,13 @@
+package com.VehicleBreakdown.Assistance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnRoadVehicleBreakdownAssistanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnRoadVehicleBreakdownAssistanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/VehicleBreakdown/Assistance/OnRoadVehicleBreakdownAssistanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.VehicleBreakdown.Assistance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnRoadVehicleBreakdownAssistanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
