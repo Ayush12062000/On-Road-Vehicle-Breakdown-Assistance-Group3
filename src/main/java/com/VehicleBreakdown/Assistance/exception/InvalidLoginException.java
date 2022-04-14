290f821125b6d1f395490b2f57532c20201a1449
Ayush Kesarwani <kaayush112@gmail.com>
2022-04-14 17:14:11 +0530

    Exception added

---
+++ src/main/java/com/VehicleBreakdown/Assistance/exception/InvalidLoginException.java
@@ -0,0 +1,10 @@
+package com.VehicleBreakdown.Assistance.exception;
+
+public class InvalidLoginException extends Exception {
+	public InvalidLoginException() {
+
+	}
+	public InvalidLoginException(String s) {
+		super(s);
+	}
+}
