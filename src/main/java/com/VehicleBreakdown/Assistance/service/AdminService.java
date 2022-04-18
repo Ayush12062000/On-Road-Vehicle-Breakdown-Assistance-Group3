8f9b36ac5eca026ea913e525da9cef8d27b12e04
ShagunChauhan <cshagun123@gmail.com>
2022-04-18 17:17:09 +0530

    Admin Controller Integration Test added

--- src/main/java/com/VehicleBreakdown/Assistance/service/AdminService.java
+++ src/main/java/com/VehicleBreakdown/Assistance/service/AdminService.java
@@ -3,6 +3,8 @@ package com.VehicleBreakdown.Assistance.service;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.http.ResponseEntity;
+
 import com.VehicleBreakdown.Assistance.model.Admin;
 import com.VehicleBreakdown.Assistance.model.Feedback;
 import com.VehicleBreakdown.Assistance.model.Mechanic;
@@ -15,5 +17,5 @@ public interface AdminService {
 	public Optional<Admin> getAdminByUsername(String username);
 	public List<Feedback> viewFeedback();
 	public List<Mechanic> getAllMechanics();
-	public String allowOrBlockMechanic(long mechanicId);
+	public ResponseEntity<String> allowOrBlockMechanic(long mechanicId);
 }
